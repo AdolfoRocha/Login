fa20eedf25e50e21d4a209795cbdd80b1fef74da
LamLu <luphuonglam@yahoo.com>
2013-02-19 17:47:09 -0800

    Initial Commit

---
+++ Login/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Login
+//
+//  Created by Lam Lu on 2/19/13.
+//  Copyright (c) 2013 Lam Lu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Login/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Login
+//
+//  Created by Lam Lu on 2/19/13.
+//  Copyright (c) 2013 Lam Lu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
