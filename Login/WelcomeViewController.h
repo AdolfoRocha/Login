6ae95404a6aa26d8b6de9abe5b6cb0f2bc430231
LamLu <luphuonglam@yahoo.com>
2013-02-21 21:33:38 -0800

    added readme.md

---
+++ Login/WelcomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  WelcomeViewController.h
+//  Login
+//
+//  Created by Lam Lu on 2/19/13.
+//  Copyright (c) 2013 Lam Lu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WelcomeViewController : UITableViewController
+
+@end
